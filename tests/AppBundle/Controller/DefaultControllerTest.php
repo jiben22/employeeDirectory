07d7e91843800de9654d0072dc882d55a739dcf1
jiben22 <benji.legallo22@live.fr>
2018-04-19 15:41:36 +0200

    Add test for login & list

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -3,16 +3,51 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    private $client = null;
+
+    public function setUp()
     {
-        $client = static::createClient();
+        $this->client = static::createClient();
+    }
 
-        $crawler = $client->request('GET', '/');
+    private function logIn()
+    {
+        $this->setUp();
+        $session = $this->client->getContainer()->get('session');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        // the firewall context (defaults to the firewall name)
+        $firewall = 'main';
+
+        $token = new UsernamePasswordToken('admin', null, $firewall, array('ROLE_ADMIN'));
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+    }
+
+    public function testLogin()
+    {
+        $this->setUp();
+        $crawler = $this->client->request('GET', '/login');
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertContains('Authentification', $crawler->filter('h2')->text());
+    }
+
+    public function testListEmployees()
+    {
+        $this->setUp();
+        $this->logIn();
+        $crawler = $this->client->request('GET', '/list');
+
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertEquals(1, $crawler->filter('h2:contains("Liste des employés")')->count());
     }
 }
